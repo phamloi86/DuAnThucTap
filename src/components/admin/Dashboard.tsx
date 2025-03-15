bb5bbefe6148f7e617d752b91b2558e01d5f0312
luong <khanhluong752004@gmail.com>
2025-03-15 13:41:30 +0700

    duong

--- src/components/admin/Dashboard.tsx
+++ src/components/admin/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
     <>
     <div>Dashboard Home</div>
     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam atque provident quas ad quod illo, ex saepe impedit numquam deserunt placeat quam, rem, eligendi repellendus optio dolorem. Iusto, quisquam deleniti!</div>
-
+    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, id? Reiciendis dicta alias qui sint ad odio nobis error ipsum consectetur rem minus pariatur laudantium vitae, vel, cupiditate est architecto.</div>
 
     </>
   )
